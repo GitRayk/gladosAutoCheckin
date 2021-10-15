644c1915d2507f4b4ddcaf31354b509f62a5da13
别七年 <57948568+GitRayk@users.noreply.github.com>
2021-10-15 17:38:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,9 @@ def getStrValue(resText, key):
 
 checkinUrl = "https://glados.network/api/user/checkin"
 statusUrl = "https://glados.network/api/user/status"
+cookie = "_ga=GA1.2.1368007815.1621871003; koa:sess=eyJjb2RlIjoiVkUwUjEtMkdQQzgtRlQ0MUwtR0JUQTgiLCJ1c2VySWQiOjgxODU2LCJfZXhwaXJlIjoxNjQ3NzkxMDMzNzEzLCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=PdCamWWhCwliK8M24rAg5wPRsCs; _gid=GA1.2.2119754023.1634187949"
 headers = {
-    "cookie": "_ga=GA1.2.1368007815.1621871003; koa:sess=eyJjb2RlIjoiVkUwUjEtMkdQQzgtRlQ0MUwtR0JUQTgiLCJ1c2VySWQiOjgxODU2LCJfZXhwaXJlIjoxNjQ3NzkxMDMzNzEzLCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=PdCamWWhCwliK8M24rAg5wPRsCs; _gid=GA1.2.2119754023.1634187949",
+    "cookie": cookie,
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
 }
 
